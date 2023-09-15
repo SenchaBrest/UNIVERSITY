71351193408ca0b1d38dabd63a7136ca240135d6
sencha <senchabrest@gmail.com>
2023-09-15 18:00:27 +0300

    LABA3 mrzis code done

---
+++ MRZIS/LABA3/perceptron.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+
+class Perceptron:
+    def __init__(self, input_size, hidden_size, output_size, learning_rate=0.1):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.learning_rate = learning_rate
+
+        # Инициализация весов для слоев
+        self.weights_input_hidden = np.random.randn(self.input_size, self.hidden_size)
+        self.bias_hidden = np.zeros((1, self.hidden_size))
+        self.weights_hidden_output = np.random.randn(self.hidden_size, self.output_size)
+        self.bias_output = np.zeros((1, self.output_size))
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def sigmoid_derivative(self, x):
+        return x * (1 - x)
+
+    def forward(self, inputs):
+        # Прямое распространение сигнала
+        self.hidden_input = np.dot(inputs, self.weights_input_hidden) + self.bias_hidden
+        self.hidden_output = self.sigmoid(self.hidden_input)
+        self.output = np.dot(self.hidden_output, self.weights_hidden_output) + self.bias_output
+        return self.output
+
+    def backward(self, inputs, target, output):
+        # Обратное распространение ошибки
+        error = target - output
+
+        # Вычисление градиентов для обновления весов
+        delta_output = error
+        delta_hidden = delta_output.dot(self.weights_hidden_output.T) * self.sigmoid_derivative(self.hidden_output)
+
+        # Обновление весов и смещений
+        self.weights_hidden_output += self.hidden_output.T.dot(delta_output) * self.learning_rate
+        self.bias_output += np.sum(delta_output, axis=0, keepdims=True) * self.learning_rate
+        self.weights_input_hidden += inputs.T.dot(delta_hidden) * self.learning_rate
+        self.bias_hidden += np.sum(delta_hidden, axis=0, keepdims=True) * self.learning_rate
+
+    def train(self, inputs, targets, epochs):
+        for epoch in range(epochs):
+            for i in range(len(inputs)):
+                input_data = np.array([inputs[i]])
+                target_data = np.array([targets[i]])
+
+                # Прямое и обратное распространение
+                output = self.forward(input_data)
+                self.backward(input_data, target_data, output)
+
+    def predict(self, inputs):
+        # Предсказание классов на основе входных данных
+        output = self.forward(inputs)
+        return output
+
+
+# Пример использования
+if __name__ == "__main__":
+    # Создание и обучение персептрона
+    input_size = 2
+    hidden_size = 4
+    output_size = 1
+    perceptron = Perceptron(input_size, hidden_size, output_size)
+
+    inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    targets = np.array([[0], [1], [1], [0]])
+
+    perceptron.train(inputs, targets, epochs=10000)
+
+    # Предсказание и вывод результатов
+    test_inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    predictions = perceptron.predict(test_inputs)
+    print(predictions)
